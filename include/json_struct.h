b31c12d2865d9dc3dcbd745ba48dea09f9f4281f
Jørgen Lind <jorgen.lind@gmail.com>
2020-09-15 19:47:40 +0200

    prefix internal macros with internal

--- include/json_struct.h
+++ include/json_struct.h
@@ -2341,9 +2341,9 @@ struct JsonStructBaseDummy
 };
 }
 
-#define JS_EXPAND(x) x
+#define JS_INTERNAL_EXPAND(x) x
 #define JS_MEMBER(member) JS::makeMemberInfo(#member, &JS_OBJECT_T::member)
-#define JS_MEMBER_ALIASES(member, ...) JS_EXPAND(JS::makeMemberInfo(#member, &JS_OBJECT_T::member, __VA_ARGS__))
+#define JS_MEMBER_ALIASES(member, ...) JS_INTERNAL_EXPAND(JS::makeMemberInfo(#member, &JS_OBJECT_T::member, __VA_ARGS__))
 #define JS_MEMBER_WITH_NAME(member, name) JS::makeMemberInfo(name, &JS_OBJECT_T::member)
 #define JS_MEMBER_WITH_NAME_AND_ALIASES(member, name, ...) JS::makeMemberInfo(name, &JS_OBJECT_T::member, __VA_ARGS__)
 
@@ -2401,92 +2401,92 @@ struct JsonStructBaseDummy
     };\
     }}
 
-#define JS_FIRST_(a, ...) a
-#define JS_SECOND_(a, b, ...) b
+#define JS_INTERNAL_FIRST_(a, ...) a
+#define JS_INTERNAL_SECOND_(a, b, ...) b
 
-#define JS_FIRST(...) JS_EXPAND(JS_FIRST_(__VA_ARGS__))
-#define JS_SECOND(...) JS_EXPAND(JS_SECOND_(__VA_ARGS__))
+#define JS_INTERNAL_FIRST(...) JS_INTERNAL_EXPAND(JS_INTERNAL_FIRST_(__VA_ARGS__))
+#define JS_INTERNAL_SECOND(...) JS_INTERNAL_EXPAND(JS_INTERNAL_SECOND_(__VA_ARGS__))
 
-#define JS_EMPTY()
+#define JS_INTERNAL_EMPTY()
 
-#define JS_EVAL(...) JS_EVAL1024(__VA_ARGS__)
-#define JS_EVAL1024(...) JS_EVAL512(JS_EVAL512(__VA_ARGS__))
-#define JS_EVAL512(...) JS_EVAL256(JS_EVAL256(__VA_ARGS__))
-#define JS_EVAL256(...) JS_EVAL128(JS_EVAL128(__VA_ARGS__))
-#define JS_EVAL128(...) JS_EVAL64(JS_EVAL64(__VA_ARGS__))
-#define JS_EVAL64(...) JS_EVAL32(JS_EVAL32(__VA_ARGS__))
-#define JS_EVAL32(...) JS_EVAL16(JS_EVAL16(__VA_ARGS__))
-#define JS_EVAL16(...) JS_EVAL8(JS_EVAL8(__VA_ARGS__))
-#define JS_EVAL8(...) JS_EVAL4(JS_EVAL4(__VA_ARGS__))
-#define JS_EVAL4(...) JS_EVAL2(JS_EVAL2(__VA_ARGS__))
-#define JS_EVAL2(...) JS_EVAL1(JS_EVAL1(__VA_ARGS__))
-#define JS_EVAL1(...) __VA_ARGS__
+#define JS_INTERNAL_EVAL(...) JS_INTERNAL_EVAL1024(__VA_ARGS__)
+#define JS_INTERNAL_EVAL1024(...) JS_INTERNAL_EVAL512(JS_INTERNAL_EVAL512(__VA_ARGS__))
+#define JS_INTERNAL_EVAL512(...) JS_INTERNAL_EVAL256(JS_INTERNAL_EVAL256(__VA_ARGS__))
+#define JS_INTERNAL_EVAL256(...) JS_INTERNAL_EVAL128(JS_INTERNAL_EVAL128(__VA_ARGS__))
+#define JS_INTERNAL_EVAL128(...) JS_INTERNAL_EVAL64(JS_INTERNAL_EVAL64(__VA_ARGS__))
+#define JS_INTERNAL_EVAL64(...) JS_INTERNAL_EVAL32(JS_INTERNAL_EVAL32(__VA_ARGS__))
+#define JS_INTERNAL_EVAL32(...) JS_INTERNAL_EVAL16(JS_INTERNAL_EVAL16(__VA_ARGS__))
+#define JS_INTERNAL_EVAL16(...) JS_INTERNAL_EVAL8(JS_INTERNAL_EVAL8(__VA_ARGS__))
+#define JS_INTERNAL_EVAL8(...) JS_INTERNAL_EVAL4(JS_INTERNAL_EVAL4(__VA_ARGS__))
+#define JS_INTERNAL_EVAL4(...) JS_INTERNAL_EVAL2(JS_INTERNAL_EVAL2(__VA_ARGS__))
+#define JS_INTERNAL_EVAL2(...) JS_INTERNAL_EVAL1(JS_INTERNAL_EVAL1(__VA_ARGS__))
+#define JS_INTERNAL_EVAL1(...) __VA_ARGS__
 
-#define JS_DEFER1(m) m JS_EMPTY()
-#define JS_DEFER2(m) m JS_EMPTY JS_EMPTY()()
+#define JS_INTERNAL_DEFER1(m) m JS_INTERNAL_EMPTY()
+#define JS_INTERNAL_DEFER2(m) m JS_INTERNAL_EMPTY JS_INTERNAL_EMPTY()()
 
-#define JS_IS_PROBE(...) JS_SECOND(__VA_ARGS__, 0)
-#define JS_PROBE() ~, 1
+#define JS_INTERNAL_IS_PROBE(...) JS_INTERNAL_SECOND(__VA_ARGS__, 0)
+#define JS_INTERNAL_PROBE() ~, 1
 
-#define JS_CAT(a,b) a ## b
+#define JS_INTERNAL_CAT(a,b) a ## b
 
-#define JS_NOT(x) JS_IS_PROBE(JS_CAT(JS__NOT_, x))
-#define JS__NOT_0 JS_PROBE()
+#define JS_INTERNAL_NOT(x) JS_INTERNAL_IS_PROBE(JS_INTERNAL_CAT(JS_INTERNAL__NOT_, x))
+#define JS_INTERNAL__NOT_0 JS_INTERNAL_PROBE()
 
-#define JS_BOOL(x) JS_NOT(JS_NOT(x))
+#define JS_INTERNAL_BOOL(x) JS_INTERNAL_NOT(JS_INTERNAL_NOT(x))
 
-#define JS_IF_ELSE(condition) JS__IF_ELSE(JS_BOOL(condition))
-#define JS__IF_ELSE(condition) JS_CAT(JS__IF_, condition)
+#define JS_INTERNAL_IF_ELSE(condition) JS_INTERNAL__IF_ELSE(JS_INTERNAL_BOOL(condition))
+#define JS_INTERNAL__IF_ELSE(condition) JS_INTERNAL_CAT(JS_INTERNAL__IF_, condition)
 
-#define JS__IF_1(...) __VA_ARGS__ JS__IF_1_ELSE
-#define JS__IF_0(...)             JS__IF_0_ELSE
+#define JS_INTERNAL__IF_1(...) __VA_ARGS__ JS_INTERNAL__IF_1_ELSE
+#define JS_INTERNAL__IF_0(...)             JS_INTERNAL__IF_0_ELSE
 
-#define JS__IF_1_ELSE(...)
-#define JS__IF_0_ELSE(...) __VA_ARGS__
+#define JS_INTERNAL__IF_1_ELSE(...)
+#define JS_INTERNAL__IF_0_ELSE(...) __VA_ARGS__
 
-#define JS_HAS_ARGS(...) JS_BOOL(JS_FIRST(JS__END_OF_ARGUMENTS_ __VA_ARGS__)())
-#define JS__END_OF_ARGUMENTS_() 0
+#define JS_INTERNAL_HAS_ARGS(...) JS_INTERNAL_BOOL(JS_INTERNAL_FIRST(JS_INTERNAL__END_OF_ARGUMENTS_ __VA_ARGS__)())
+#define JS_INTERNAL__END_OF_ARGUMENTS_() 0
 
-#define JS_MAP_MEMBER(m, first, ...)              \
+#define JS_INTERNAL_MAP_MEMBER(m, first, ...)              \
   m(#first, &JS_OBJECT_T::first)           \
-  JS_IF_ELSE(JS_HAS_ARGS(__VA_ARGS__))(,   \
-    JS_DEFER2(JS__MAP_MEMBER)()(m, __VA_ARGS__)   \
+  JS_INTERNAL_IF_ELSE(JS_INTERNAL_HAS_ARGS(__VA_ARGS__))(,   \
+    JS_INTERNAL_DEFER2(JS_INTERNAL__MAP_MEMBER)()(m, __VA_ARGS__)   \
   )(                                       \
     /* Do nothing, just terminate */       \
   )
-#define JS__MAP_MEMBER() JS_MAP_MEMBER
+#define JS_INTERNAL__MAP_MEMBER() JS_INTERNAL_MAP_MEMBER
 
-#define JS_MAKE_MEMBERS(...) JS_EXPAND(JS_EVAL(JS_MAP_MEMBER(JS::makeMemberInfo, __VA_ARGS__)))
+#define JS_INTERNAL_MAKE_MEMBERS(...) JS_INTERNAL_EXPAND(JS_INTERNAL_EVAL(JS_INTERNAL_MAP_MEMBER(JS::makeMemberInfo, __VA_ARGS__)))
 #define JS_OBJ(...) \
     template<typename JS_OBJECT_T> \
     struct JsonStructBase \
     { \
-       using TT = decltype(JS::makeTuple(JS_MAKE_MEMBERS(__VA_ARGS__))); \
+       using TT = decltype(JS::makeTuple(JS_INTERNAL_MAKE_MEMBERS(__VA_ARGS__))); \
        static const TT &js_static_meta_data_info() \
-       { static auto ret = JS::makeTuple(JS_MAKE_MEMBERS(__VA_ARGS__)); return ret; } \
+       { static auto ret = JS::makeTuple(JS_INTERNAL_MAKE_MEMBERS(__VA_ARGS__)); return ret; } \
        static const decltype(JS::makeTuple()) &js_static_meta_super_info() \
        { static auto ret = JS::makeTuple(); return ret; } \
     }
 
-#define JS_MAP_SUPER(m, first, ...)              \
+#define JS_INTERNAL_MAP_SUPER(m, first, ...)              \
   m<first>(#first)                              \
-  JS_IF_ELSE(JS_HAS_ARGS(__VA_ARGS__))(,         \
-    JS_DEFER2(JS__MAP_SUPER)()(m, __VA_ARGS__)   \
+  JS_INTERNAL_IF_ELSE(JS_INTERNAL_HAS_ARGS(__VA_ARGS__))(,         \
+    JS_INTERNAL_DEFER2(JS_INTERNAL__MAP_SUPER)()(m, __VA_ARGS__)   \
   )(                                             \
     /* Do nothing, just terminate */             \
   )
-#define JS__MAP_SUPER() JS_MAP_SUPER
+#define JS_INTERNAL__MAP_SUPER() JS_INTERNAL_MAP_SUPER
 
-#define JS_MAKE_SUPER_CLASSES(...) JS_EXPAND(JS_EVAL(JS_MAP_SUPER(JS::makeSuperInfo, __VA_ARGS__)))
-#define JS_SUPER(...) JS::makeTuple(JS_EXPAND(JS_MAKE_SUPER_CLASSES(__VA_ARGS__)))
+#define JS_INTERNAL_MAKE_SUPER_CLASSES(...) JS_INTERNAL_EXPAND(JS_INTERNAL_EVAL(JS_INTERNAL_MAP_SUPER(JS::makeSuperInfo, __VA_ARGS__)))
+#define JS_SUPER(...) JS::makeTuple(JS_INTERNAL_EXPAND(JS_INTERNAL_MAKE_SUPER_CLASSES(__VA_ARGS__)))
 
 #define JS_OBJ_SUPER(super_list, ...) \
     template<typename JS_OBJECT_T> \
     struct JsonStructBase \
     { \
-       using TT = decltype(JS::makeTuple(JS_EXPAND(JS_MAKE_MEMBERS(__VA_ARGS__)))); \
+       using TT = decltype(JS::makeTuple(JS_INTERNAL_EXPAND(JS_INTERNAL_MAKE_MEMBERS(__VA_ARGS__)))); \
        static const TT &js_static_meta_data_info() \
-       { static auto ret = JS::makeTuple(JS_EXPAND(JS_MAKE_MEMBERS(__VA_ARGS__))); return ret; } \
+       { static auto ret = JS::makeTuple(JS_INTERNAL_EXPAND(JS_INTERNAL_MAKE_MEMBERS(__VA_ARGS__))); return ret; } \
         static const decltype(super_list) &js_static_meta_super_info() \
         { static auto ret = super_list; return ret; } \
     }
@@ -2497,9 +2497,9 @@ struct JsonStructBaseDummy
     template<typename JS_OBJECT_T> \
     struct JsonStructBaseDummy<Type, JS_OBJECT_T> \
     { \
-       using TT = decltype(JS::makeTuple(JS_MAKE_MEMBERS(__VA_ARGS__))); \
+       using TT = decltype(JS::makeTuple(JS_INTERNAL_MAKE_MEMBERS(__VA_ARGS__))); \
        static const TT &js_static_meta_data_info() \
-       { static auto ret = JS::makeTuple(JS_MAKE_MEMBERS(__VA_ARGS__)); return ret; } \
+       { static auto ret = JS::makeTuple(JS_INTERNAL_MAKE_MEMBERS(__VA_ARGS__)); return ret; } \
        static const decltype(JS::makeTuple()) &js_static_meta_super_info() \
        { static auto ret = JS::makeTuple(); return ret; } \
     };\
@@ -2511,9 +2511,9 @@ struct JsonStructBaseDummy
     template<typename JS_OBJECT_T> \
     struct JsonStructBaseDummy<Type, JS_OBJECT_T> \
     { \
-       using TT = decltype(JS::makeTuple(JS_EXPAND(JS_MAKE_MEMBERS(__VA_ARGS__)))); \
+       using TT = decltype(JS::makeTuple(JS_INTERNAL_EXPAND(JS_INTERNAL_MAKE_MEMBERS(__VA_ARGS__)))); \
        static const TT &js_static_meta_data_info() \
-       { static auto ret = JS::makeTuple(JS_EXPAND(JS_MAKE_MEMBERS(__VA_ARGS__))); return ret; } \
+       { static auto ret = JS::makeTuple(JS_INTERNAL_EXPAND(JS_INTERNAL_MAKE_MEMBERS(__VA_ARGS__))); return ret; } \
         static const decltype(super_list) &js_static_meta_super_info() \
         { static auto ret = super_list; return ret; } \
     };\
