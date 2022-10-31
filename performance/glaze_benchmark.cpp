96cca41a531af09361c1248c13746a121ff81f68
Jørgen Lind <jorgen.lind@gmail.com>
2022-10-31 22:35:21 +0100

    Update benchmark

--- performance/glaze_benchmark.cpp
+++ performance/glaze_benchmark.cpp
@@ -1,15 +1,11 @@
-#include "assert.h"
-#include "generated.json.h"
+#define JS_STL_ARRAY 1
 #include <json_struct/json_struct.h>
-#include <chrono>
 
 #include "catch2/catch.hpp"
 
-#include "include/rapidjson/document.h"
-#include "include/simdjson/simdjson.h"
-#include "include/nlohmann/json.hpp"
-
-static constexpr std::string_view message = R"(
+namespace
+{
+static const char json[] = R"(
 {
    "fixed_object": {
       "int_array": [0, 1, 2, 3, 4, 5, 6],
@@ -41,10 +37,6 @@ static constexpr std::string_view message = R"(
    "another_bool": false
 }
 )";
-
-#include <chrono>
-#include <iostream>
-#include <unordered_map>
 
 struct fixed_object_t
 {
@@ -87,9 +79,7 @@ struct obj_t
    bool boolean{};
    bool another_bool{};
 };
-
-#define JS_STL_ARRAY 1
-#include "json_struct/json_struct.h"
+}
 
 JS_OBJ_EXT(fixed_object_t, int_array, float_array, double_array);
 JS_OBJ_EXT(fixed_name_object_t, name0, name1, name2, name3, name4);
@@ -97,73 +87,32 @@ JS_OBJ_EXT(nested_object_t, v3s, id);
 JS_OBJ_EXT(another_object_t, string, another_string, boolean, nested_object);
 JS_OBJ_EXT(obj_t, fixed_object, fixed_name_object, another_object, string_array, string, number, boolean, another_bool);
 
-#ifdef NDEBUG
-static constexpr size_t iterations = 1'000'000;
-#else
-static constexpr size_t iterations = 100'000;
-#endif
-
-TEST_CASE("GlazeTestjson_struct", "[performance]")
+TEST_CASE("glaze_json_benchmark", "[performance]")
 {
-   std::string buffer{ message };
 
-   obj_t obj;
-
-   auto t0 = std::chrono::steady_clock::now();
-
-   for (size_t i = 0; i < iterations; ++i) {
-     JS::ParseContext context(buffer);
+   BENCHMARK("Parse Glaze Json")
+   {
+     obj_t obj;
+     JS::ParseContext context(json);
      context.track_member_assignement_state = false;
-     context.parseTo(obj);
-     if (context.error != JS::Error::NoError)
+     auto error = context.parseTo(obj);
+     if (error != JS::Error::NoError)
      {
-      std::cout << "json_struct error: " << context.makeErrorString() << '\n';
+      fprintf(stderr, "json_struct error: %s\n", context.makeErrorString().c_str());
      }
-     buffer.clear();
-     buffer = JS::serializeStruct(obj);
-   }
-   auto t1 = std::chrono::steady_clock::now();
+     return obj;
+   };
 
-   auto runtime = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count() * 1e-6;
-
-   std::cout << "json_struct roundtrip runtime: " << runtime << '\n';
-   
-   // write performance
-   t0 = std::chrono::steady_clock::now();
-   
-   for (size_t i = 0; i < iterations; ++i) {
-      JS::ParseContext context(buffer);
-      context.parseTo(obj);
-   }
-   
-   t1 = std::chrono::steady_clock::now();
-   
-   runtime = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count() * 1e-6;
-   
-   auto mbytes_per_sec = iterations * buffer.size() / (runtime * 1048576);
-   std::cout << "json_struct write_json size: " << buffer.size() << " bytes\n";
-   std::cout << "json_struct write_json: " << runtime << " s, " << mbytes_per_sec
-             << " MB/s"
-             << "\n";
-   
-   // read performance
-   
-   t0 = std::chrono::steady_clock::now();
-   
-   for (size_t i = 0; i < iterations; ++i) {
-      buffer.clear();
-      buffer = JS::serializeStruct(obj);
-   }
-   
-   t1 = std::chrono::steady_clock::now();
-   
-   runtime = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count() * 1e-6;
-   
-   mbytes_per_sec = iterations * buffer.size() / (runtime * 1048576);
-   std::cout << "json_struct read_json size: " << buffer.size() << " bytes\n";
-   std::cout << "json_struct read_json: " << runtime << " s, " << mbytes_per_sec
-             << " MB/s"
-             << "\n";
-   
-   std::cout << '\n';
+   BENCHMARK_ADVANCED("Serialize Glaze Json")(Catch::Benchmark::Chronometer meter)
+   {
+     obj_t obj;
+     JS::ParseContext context(json);
+     context.track_member_assignement_state = false;
+     auto error = context.parseTo(obj);
+     if (error != JS::Error::NoError)
+     {
+       fprintf(stderr, "json_struct error: %s\n", context.makeErrorString().c_str());
+     }
+     meter.measure([&obj] { return JS::serializeStruct(obj); });
+   };
 }
