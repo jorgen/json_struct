667bce9a3c4d9b20fd4cc88aa5f3e1bf777181d9
Jørgen Lind <jorgen.lind@gmail.com>
2023-12-26 10:48:42 +0100

    Make clusterfuzzlite compile on my machine

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -eu
 # Copy fuzzer executables to $OUT/
-$CXX $CFLAGS $LIB_FUZZING_ENGINE \
-  $SRC/json_struct/.clusterfuzzlite/reformat_fuzzer.cpp \
-  -o $OUT/reformat_fuzzer \
-  -I$SRC/json_struct/include
+$CXX $CXXFLAGS -fsanitize=fuzzer $SRC/json_struct/.clusterfuzzlite/reformat_fuzzer.cpp -o $OUT/reformat_fuzzer -I$SRC/json_struct/include
