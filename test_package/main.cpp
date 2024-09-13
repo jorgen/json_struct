addc61f5ec0a52e53491639f0f750463451412ff
Arijan Amigh <amigharijan@gmail.com>
2024-09-13 15:28:59 +0200

    Add conanfile and test_package

---
+++ test_package/main.cpp
@@ -0,0 +1,19 @@
+#include "json_struct/json_struct.h"
+#include <iostream>
+
+struct MyTestStruct
+{
+    std::string name;
+    unsigned age;
+    JS_OBJ(name, age);
+};
+
+int main()
+{
+    MyTestStruct person{.name="Jonh", .age=23};
+
+    std::string person_json = JS::serializeStruct(person);
+    std::cout << person_json << std::endl;
+
+    return 0;
+}
